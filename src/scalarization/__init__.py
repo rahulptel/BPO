f38a0fb1de4bb698d6a4dd48e37462ed5a4397eb
rahulptel <rahul.polymtl@gmail.com>
2025-11-06 13:59:11 -0500

    Remove build aug cheby

--- src/scalarization/__init__.py
+++ src/scalarization/__init__.py
@@ -1,11 +1,9 @@
 from .aug_cheby.mokp import (  # SCIPAugChebyMOKPScalarizer,; DocplexAugChebyMOKPScalarizer,
     AugChebyMOKPScalarizer,
-    build_aug_cheby_scalarizer,
 )
 
 __all__ = [
     "AugChebyMOKPScalarizer",
     # "SCIPAugChebyMOKPScalarizer",
     # "DocplexAugChebyMOKPScalarizer",
-    "build_aug_cheby_scalarizer",
 ]
--- src/scalarization/aug_cheby/__init__.py
+++ src/scalarization/aug_cheby/__init__.py
@@ -1,11 +1,9 @@
 from .mokp import (  # SCIPAugChebyMOKPScalarizer,; DocplexAugChebyMOKPScalarizer,
     AugChebyMOKPScalarizer,
-    build_aug_cheby_scalarizer,
 )
 
 __all__ = [
     "AugChebyMOKPScalarizer",
     # "SCIPAugChebyMOKPScalarizer",
     # "DocplexAugChebyMOKPScalarizer",
-    "build_aug_cheby_scalarizer",
 ]
