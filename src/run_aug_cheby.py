8da2bd3b4eb404c16c6e5c30ce75fdfda811b61f
rahulptel <rahul.polymtl@gmail.com>
2025-11-07 18:14:55 -0500

    Create env when necessary

--- src/run_aug_cheby.py
+++ src/run_aug_cheby.py
@@ -10,14 +10,22 @@ torch.set_default_dtype(torch.float64)
 
 @hydra_main(config_path="configs", config_name="run_aug_cheby", version_base=None)
 def main(cfg):
-    env = build_gurobi_env(time_limit=cfg.time_limit)
-    env.start()
+    env = (
+        build_gurobi_env(time_limit=cfg.time_limit)
+        if cfg.scalarization.optimizer == "gurobi"
+        else None
+    )
+    if env is not None:
+        env.start()
+
     for pid in range(cfg.from_pid, cfg.to_pid):
         cfg.problem.iseed = pid
         instance = build_instance(cfg.problem, env)
         solver = AugChebySolver(cfg, instance, env=env)
         solver.run()
-    env.dispose()
+
+    if env is not None:
+        env.dispose()
 
 
 if __name__ == "__main__":
